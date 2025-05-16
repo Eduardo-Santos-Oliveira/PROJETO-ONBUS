a2ab2d2d14d39d364248c1fad21cc5bd53a7c821
Eduardo-Santos-Oliveira <eduardo.santos.oliveira.exe@gmail.com>
2025-05-16 09:43:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,11 +15,18 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  host: process.env.PGHOST || 'postgres.railway.internal',
+  port: process.env.PGPORT || 5432,
+  user: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB || 'railway',
+  ssl: false // Desative SSL para conexão interna no Railway
 });
 
 module.exports = pool;
+
 
 let linhasData = [];
 try {
