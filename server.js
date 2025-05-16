9ecef850d2bda94b8e2ee841946af08c0026aac3
Eduardo-Santos-Oliveira <eduardo.santos.oliveira.exe@gmail.com>
2025-05-16 04:46:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 const cors = require('cors');
 const { Pool } = require('pg');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
